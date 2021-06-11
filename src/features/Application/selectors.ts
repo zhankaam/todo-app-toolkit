228192a036acbcfcebae76dbe1b80932b6becdbc
zhankaam <zhankaam@gmail.com>
2021-06-11 22:21:29 +0300

    Updates

--- src/features/Application/selectors.ts
+++
@@ -1,4 +0,0 @@
-import {AppRootStateType} from '../../utils/types'
-
-export const selectStatus = (state: AppRootStateType) => state.app.status
-export const selectIsInitialized = (state: AppRootStateType) => state.app.isInitialized
